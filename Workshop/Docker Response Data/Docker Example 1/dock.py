2322c2a87686d9e6c373a8a9df9a9ebf64dccf75
NetAcad DEVASC <changeme@example.com>
2023-01-19 11:10:44 +0000

    Laatste versie

---
+++ Workshop/Docker Response Data/Docker Example 1/dock.py
@@ -0,0 +1,14 @@
+import json
+with open ('Workshop/Docker Response Data/Docker Example 1/data.json', 'r') as docker_json_file:
+    docker_dict = json.load (docker_json_file)
+print("---------1--------")
+print("Converting json string to dict, and showing keys at level 1")
+print(docker_dict[0].keys())
+print("---------2--------")
+print("Converting dict to raw json")
+docker_json = json.dumps(docker_dict)
+print("---------3--------")
+print("Filtering from dict")
+print(docker_dict[0]["Created"])
+print(docker_dict[0]["Architecture"])
+print(docker_dict[0]["Os"])
---
+++ Workshop/Webex API Response Data/webdata.py
@@ -0,0 +1,10 @@
+import json
+with open ('Workshop/Webex API Response Data/data.json', 'r') as json_file:
+    response_json = json.load (json_file)
+#DISPLAY FILTERED RESULTS 
+print("Displaying partial information")
+#print(type(res))
+print("Name: " + response_json['displayName'])
+print("Created: " + response_json['created'])
+print("User Type: " + response_json['type'])
+print("User Status: " + response_json['status'])
